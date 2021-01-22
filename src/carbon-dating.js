c72d4c90e83fbc0cf8ca849f51af3dff128a0ba3
VitaliyStepanovich <vitaliys.garshin@gmail.com>
2021-01-22 21:54:07 +0300

    init: start basic-js task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -5,5 +5,6 @@ const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(/* sampleActivity */) {
   throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+
+  
 };
