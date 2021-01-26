5c3d79ccccc59df6baccb3dd6e02cd00f94fb049
VitaliyStepanovich <vitaliys.garshin@gmail.com>
2021-01-26 13:11:43 +0300

    feat: add solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+
+  if (!Array.isArray(members)) { return false; }
+  
+  let names = [];
+
+	//Define if it`s string and return array with transformed Uppercase strings 
+	names = members.map(el => typeof el === 'string' ? String(el).toUpperCase().trim() : '');
+	
+  //Sort array and grab first letters
+  names = names.sort().reduce((result, item) => result + item.substring(0, 1), '')
+  
+	return names;
 };
