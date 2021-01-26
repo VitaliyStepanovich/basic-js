156f6a22239b6f7e721281eeed84726d2d48e5a7
VitaliyStepanovich <vitaliys.garshin@gmail.com>
2021-01-26 13:18:00 +0300

    refactor: correct indents

--- src/dream-team.js
+++ src/dream-team.js
@@ -5,12 +5,11 @@ module.exports = function createDreamTeam(members) {
   if (!Array.isArray(members)) { return false; }
   
   let names = [];
-
-	//Define if it`s string and return array with transformed Uppercase strings 
-	names = members.map(el => typeof el === 'string' ? String(el).toUpperCase().trim() : '');
-	
+  
+  //Define if it`s string and return array with transformed Uppercase strings 
+  names = members.map(el => typeof el === 'string' ? String(el).toUpperCase().trim() : '');	
   //Sort array and grab first letters
   names = names.sort().reduce((result, item) => result + item.substring(0, 1), '')
   
-	return names;
+  return names;
 };
