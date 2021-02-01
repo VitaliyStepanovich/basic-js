c7bef3c0423641fdd88aaeb2bd836f5e161aca6e
VitaliyStepanovich <vitaliys.garshin@gmail.com>
2021-02-01 16:10:11 +0300

    feat: add solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,39 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+
+  if (!Array.isArray(arr)) {
+    throw new Error();
+  } 
+
+  const sequences = ['--discard-next', '--discard-prev', '--double-next', '--double-prev'];
+  let transformArr = [...arr];
+
+  transformArr =  transformArr.reduce((acc, value, index, array) => {	
+    switch(true) {
+      case value == sequences[0]:
+      array[index + 1] !== undefined  ? array.splice(index + 1, 1) : null;
+      break;
+
+    case value == sequences[1]:
+      array[index - 1] !== undefined ? array.splice(index - 1, 1) : null;	 
+      break;
+
+    case value == sequences[2]:
+      array[index + 1] !== undefined ? array[index] = array[index + 1] : null;	 
+      break;		
+
+    case value == sequences[3]:
+      array[index - 1] !== undefined ? array[index] = array[index - 1] : null;	 
+      break;
+  
+    default:   
+      break;	  
+  }	
+
+  return array;
+  }, []);	
+
+  return transformArr.filter(item => !sequences.includes(item));
+
+}
