8a21553a569140f2d74429e140a21cf364e281d0
VitaliyStepanovich <vitaliys.garshin@gmail.com>
2021-02-02 13:46:13 +0300

    feat: add solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,8 +3,18 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
+module.exports = function dateSample(sampleActivity) {
+  if (typeof(sampleActivity) === 'string') { 
 
+  const sample = Number(sampleActivity);
+
+  if (isNaN(sample) || sample > MODERN_ACTIVITY || sample <= 0 ) {
+    return false;
+  }
+
+  let result = Math.log(MODERN_ACTIVITY / sample) / (0.693 / HALF_LIFE_PERIOD);
+  return Math.ceil(result); 
+  
+  } else { return false;}
   
 };
