a1812459bbd0140cf8827306a58bc0eb7fb197cc
VitaliyStepanovich <vitaliys.garshin@gmail.com>
2021-02-20 16:03:08 +0300

    feat: add solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -5,9 +5,10 @@ module.exports = function createDreamTeam(members) {
   if (!Array.isArray(members)) { return false; }
   
   let names = [];
-  
+
   //Define if it`s string and return array with transformed Uppercase strings 
-  names = members.map(el => typeof el === 'string' ? String(el).toUpperCase().trim() : '');	
+  names = members.map(el => typeof el === 'string' ? String(el).toUpperCase().trim() : '');
+	
   //Sort array and grab first letters
   names = names.sort().reduce((result, item) => result + item.substring(0, 1), '')
   
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function repeater(str, options) {
+  //throw new CustomError('Not implemented');
+  let result = '';
+
+  let repeatTimes = options.repeatTimes || 1;  
+  let separator = options.separator || '+';
+  let addition = options.addition !== undefined  ? options.addition : '';
+  let additionRepeatTimes = options.additionRepeatTimes || 1;
+  let additionSeparator = options.additionSeparator || '|'
   
+  for (let i = 0; i < repeatTimes; i++)  {
+    
+    result += i > 0 ? separator : '';
+    result += str;    
+    
+    for (let j = 0; j < additionRepeatTimes; j++)  {
+      result += j > 0 ? additionSeparator : '';
+      result += addition;
+    }
+
+  }  
+ return result;
+
+}
